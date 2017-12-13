50c0d299ece97c61061836480cb44e0c5de96e66
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-12-13 23:53:16 +0100

    Some tensorflow work

--- src/main/kotlin/kaifu2x.kt
+++ src/main/kotlin/kaifu2x.kt
@@ -13,12 +13,12 @@ import com.soywiz.korio.serialization.json.Json
 import com.soywiz.korio.util.clamp
 import com.soywiz.korio.vfs.LocalVfs
 import com.soywiz.korio.vfs.resourcesVfs
-import org.tensorflow.Graph
-import org.tensorflow.Session
-import org.tensorflow.Tensor
-import org.tensorflow.TensorFlow
+import org.tensorflow.*
 import kotlin.math.max
 import kotlin.math.min
+import org.tensorflow.Tensor
+
+
 
 inline fun <T : AutoCloseable, TR> T.auto(callback: (T) -> TR): TR {
 	try {
@@ -28,27 +28,124 @@ inline fun <T : AutoCloseable, TR> T.auto(callback: (T) -> TR): TR {
 	}
 }
 
-fun tfdemo() {
-	Graph().auto { g ->
-		val value = "Hello from " + TensorFlow.version()
+//fun <T> Output<T>.
+fun Graph.binaryOp(type: String, l: Output<*>, r: Output<*>): Output<*> {
+	return opBuilder(type, type).addInput(l).addInput(r).build().output<Any>(0)
+}
 
-		// Construct the computation graph with a single operation, a constant
-		// named "MyConst" with a value "value".
-		Tensor.create(value.toByteArray(UTF8)).auto { t ->
-			// The Java API doesn't yet include convenience functions for adding operations.
-			g.opBuilder("Const", "MyConst")
-				.setAttr("dtype", t.dataType())
+// https://github.com/tensorflow/tensorflow/blob/r1.4/tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java
+fun Graph.div(x: Output<*>, y: Output<*>): Output<*> = binaryOp("Div", x, y)
+fun Graph.mul(x: Output<*>, y: Output<*>): Output<*> = binaryOp("Mul", x, y)
+fun Graph.sub(x: Output<*>, y: Output<*>): Output<*> = binaryOp("Sub", x, y)
+fun Graph.add(x: Output<*>, y: Output<*>): Output<*> = binaryOp("Add", x, y)
+
+interface IOutput<T> {
+	val output: Output<T>
+}
+data class NamedOutput<T>(val name: String, override val output: Output<T>) : IOutput<T> {
+	constructor(name: String, output: IOutput<T>) : this(name, output.output)
+}
+data class UnnamedOutput<T>(override val output: Output<T>) : IOutput<T>
+fun <T> IOutput<T>.named(name: String) = NamedOutput(name, this.output)
+
+class GraphBuilder(val g: Graph) {
+	var lastId: Int = 0
+	operator fun IOutput<*>.div(that: IOutput<*>): IOutput<*> = UnnamedOutput(g.div(this.output, that.output))
+	operator fun IOutput<*>.times(that: IOutput<*>): IOutput<*> = UnnamedOutput(g.mul(this.output, that.output))
+	operator fun IOutput<*>.plus(that: IOutput<*>): IOutput<*> = UnnamedOutput(g.add(this.output, that.output))
+	operator fun IOutput<*>.minus(that: IOutput<*>): IOutput<*> = UnnamedOutput(g.sub(this.output, that.output))
+
+	fun <T> constant(name: String, t: Tensor<T>): Operation {
+		return g.opBuilder("Const", name).setAttr("dtype", t.dataType()).setAttr("value", t).build()
+	}
+
+	inline fun <TR> tensor(value: ByteArray, callback: (Tensor<*>) -> TR): TR {
+		return Tensor.create(value).auto { t -> callback(t) }
+	}
+
+	inline fun <TR> session(callback: Session.() -> TR): TR {
+		return Session(g).auto { callback(it) }
+	}
+
+	inline fun <TR> Session.runner(callback: Session.Runner.() -> TR): TR {
+		return callback(runner())
+	}
+
+	fun <T> constant(name: String, value: Any, type: Class<T>): UnnamedOutput<T> {
+		return Tensor.create(value, type).auto { t ->
+			UnnamedOutput<T>(g.opBuilder("Const", name)
+				.setAttr("dtype", DataType.fromClass(type))
 				.setAttr("value", t)
 				.build()
+				.output(0))
 		}
+	}
 
-		// Execute the "MyConst" operation in a Session.
-		Session(g).auto { s ->
-			s.runner().fetch("MyConst").run().get(0).auto { output ->
-				println(output.bytesValue().toString(UTF8))
+	fun Session.Runner.fetch(named: IOutput<*>) = this.fetch(named.output.op().name())
+	fun Session.Runner.fetchRun(named: IOutput<*>) = fetch(named).run()
+	fun Session.Runner.fetchRunFirst(named: IOutput<*>) = fetchRun(named).first()
+
+	fun allocName() = "Auto${lastId++}"
+
+	fun constant(value: ByteArray): NamedOutput<String> = allocName().run { constant(this, value).named(this) }
+	fun constant(value: Int): NamedOutput<Int> = allocName().run { constant(this, value).named(this) }
+	fun constant(value: Float): NamedOutput<Float> = allocName().run { constant(this, value).named(this) }
+	fun constant(value: IntArray): NamedOutput<Int> = allocName().run { constant(this, value).named(this) }
+	fun constant(value: FloatArray): NamedOutput<Float> = allocName().run { constant(this, value).named(this) }
+
+	fun constant(name: String, value: ByteArray): NamedOutput<String> = this.constant(name, value, String::class.java).named(name)
+
+	fun constant(name: String, value: Int): NamedOutput<Int> = this.constant(name, value, Int::class.java).named(name)
+	fun constant(name: String, value: IntArray): NamedOutput<Int> = this.constant(name, value, Int::class.java).named(name)
+
+	fun constant(name: String, value: Float): NamedOutput<Float> = this.constant(name, value, Float::class.java).named(name)
+	fun constant(name: String, value: FloatArray): NamedOutput<Float> = this.constant(name, value, Float::class.java).named(name)
+}
+
+inline fun <T> Graph.builder(callback: GraphBuilder.() -> T): T {
+	return callback(GraphBuilder(this))
+}
+
+inline fun <T> graph(callback: GraphBuilder.() -> T): T {
+	return Graph().auto { g -> callback(GraphBuilder(g)) }
+}
+
+fun tfdemo() {
+	graph {
+		val value = "Hello from " + TensorFlow.version()
+
+		val const1 = constant(value.toByteArray(UTF8))
+		val const2 = constant(value.toByteArray(UTF8))
+
+		session {
+			runner {
+				fetchRunFirst(const1 + const2).auto { output ->
+					println(output.bytesValue().toString(UTF8))
+				}
 			}
 		}
 	}
+
+	//Graph().auto { g ->
+	//	val value = "Hello from " + TensorFlow.version()
+//
+	//	// Construct the computation graph with a single operation, a constant
+	//	// named "MyConst" with a value "value".
+	//	Tensor.create(value.toByteArray(UTF8)).auto { t ->
+	//		// The Java API doesn't yet include convenience functions for adding operations.
+	//		g.opBuilder("Const", "MyConst")
+	//			.setAttr("dtype", t.dataType())
+	//			.setAttr("value", t)
+	//			.build()
+	//	}
+//
+	//	// Execute the "MyConst" operation in a Session.
+	//	Session(g).auto { s ->
+	//		s.runner().fetch("MyConst").run().get(0).auto { output ->
+	//			println(output.bytesValue().toString(UTF8))
+	//		}
+	//	}
+	//}
 }
 
 fun main(args: Array<String>) = Korio {
